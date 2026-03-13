9df1bea5ad8bfd9cbe2f68ad77ab0b1384b23f3c
Megan Uyen Nguyen <megannguyen@berkeley.edu>
2026-03-13 11:45:39 -0700

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,11 @@
 # Our Hello World file
 
 print("Hello World")
+
+# git init    # this initializes an empty Git repository for your project folder.
+
+# git add.    # this adds all the files in your project folder to be tracked by Git.
+
+# git remote add origin https://github.com/your_github_username/your_project_repo_link_here.git
+
+# git push origin master
